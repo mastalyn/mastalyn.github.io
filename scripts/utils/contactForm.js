0af8fa4e9498a774cd1b546fe5eb4a842d832f38
mastalyn <mastalyn010203@gmail.com>
2023-02-10 16:43:38 +0100

    Update contactForm.js

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,10 +1,13 @@
 const form = document.querySelector("form");
+const main = document.querySelector(".main");
 const modalBg = document.getElementById("contact_modal");
 const inputs = document.querySelectorAll(
   'input[type="text"], input[type="email"]'
 );
 
 let first, last, email, textarea;
+
+console.log(inputs);
 
 const addGlobalEventListener = (type, selector, callback, options) => {
   document.addEventListener(
@@ -17,6 +20,7 @@ const addGlobalEventListener = (type, selector, callback, options) => {
 };
 
 addGlobalEventListener("click", "#displayModal", (event) => {
+  console.log("Clicked Button");
   displayModal(event);
 });
 
@@ -40,11 +44,32 @@ const errorDisplay = (tag, message, valid) => {
 
 // Vérification des inputs
 const firstChecker = (value) => {
+  console.log(value);
   if (value.length > 0 && (value.length < 3 || value.length > 20)) {
-    errorDisplay("first", "Le prénom doit faire entre 3 et 20 caractères");
+    errorDisplay("first", "Le pseudo doit faire entre 3 et 20 caractères");
+  } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+    errorDisplay(
+      "first",
+      "Le prénom ne doit pas contenir de caractères spéciaux"
+    );
+   
   } else {
     errorDisplay("first", "", true);
     first = value;
+  }
+};
+
+const lastChecker = (value) => {
+  console.log(value);
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay("last", "Le pseudo doit faire entre 3 et 20 caractères");
+    
+  } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+    errorDisplay("last", "Le nom ne doit pas contenir de caractères spéciaux");
+    
+  } else {
+    errorDisplay("last", "", true);
+    last = value;
   }
 };
 
@@ -59,17 +84,8 @@ const emailChecker = (value) => {
   }
 };
 
-const lastChecker = (value) => {
-  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
-    errorDisplay("last", "Le nom doit faire entre 3 et 20 caractères");
-   
-  } else {
-    errorDisplay("last", "", true);
-    last = value;
-  }
-};
-
 const textareaChecker = (value) => {
+  console.log(value);
   if (value.length === 0) {
     errorDisplay("textarea", "Le message ne peut pas être vide");
     textarea = null;
@@ -94,44 +110,48 @@ inputs.forEach((input) => {
       case "textarea":
         textareaChecker(e.target.value);
         break;
-     
+      default:return;
+        
     }
   });
+});
+
+// Fonction pour vérifier l'envoi du formulaire
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("test");
+  if (first && last && email && textarea) {
+    const data = {
+      first,
+      last,
+      email,
+      textarea,
+    };
+    console.log(data);
+
+    inputs.forEach((input) => (input.value = ""));
+
+    first = null;
+    last = null;
+    email = null;
+    main.style.display = "block";
+    closeModal()
+    
+  } else {
+    
+    alert("Veuillez remplir correctement les champs");
+  }
+
+ 
 });
 
 function closeModal() {
   modalBg.style.display = "none";
 }
 
-// Keyboard
-document.addEventListener("keyup", (e) => {
-  if (e.key === "Escape") {
-    closeModal();
-  }
-});
+// Clavier
+modalBg.onkeydown = function (event) {
+  if (event.key === "Escape") closeModal();
+};
 
 
-// Fonction pour vérifier l'envoi du formulaire
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (first && last && email && textarea) {
-    e.preventDefault();
-    const formData = new FormData(form);
-
-    const data = {};
-    for (const [key, value] of formData) {
-      data[key] = value;
-    }
-    console.log(data);
-
-    inputs.forEach((input) => (input.value = ""));
-    modalBg.style.display = "none";
-    
-    closeModal()
-    
-      } else {
-    
-    alert("Veuillez remplir correctement les champs");
-    
-  }
-});
