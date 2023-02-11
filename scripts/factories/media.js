fd8ee990ed31c1473085ac42707c3e3d7d570dc6
mastalyn <mastalyn010203@gmail.com>
2023-02-11 11:17:20 +0100

    Update media.js

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -3,6 +3,7 @@ function mediaFactory(mediaData, photographer) {
   const { price, likes, image, video, title, date, id } = mediaData;
 
   const mediaImage = `assets/photographers/medias/${image}`;
+  // console.log("mediaImage", mediaImage);
   const mediaVideo = `assets/photographers/medias/${video}`;
   const like = `${likes}`;
   const dates = `${date}`;
@@ -29,7 +30,7 @@ function mediaFactory(mediaData, photographer) {
     mediaContainer.setAttribute("data-likes", like);
     mediaContainer.setAttribute("data-date", dates);
     mediaContainer.setAttribute("data-title", titles);
-     mediaLikesButtonImage.dataset.id=mediaId
+    mediaLikesButtonImage.dataset.id = mediaId;
 
     // Pop method for media
     // In MP4 case
@@ -64,11 +65,14 @@ function mediaFactory(mediaData, photographer) {
     const mediaImg = document.createElement("img");
     const mediaVid = document.createElement("video");
 
-    mediaImg.setAttribute("src", mediaImage);
+    if (!mediaImage.includes("undefined")) {
+      mediaImg.setAttribute("src", mediaImage);
+    } else {
+      mediaVid.setAttribute("src", mediaVideo);
+    }
     mediaImg.setAttribute("class", "medias");
     mediaImg.setAttribute("mediaId", mediaId);
     mediaImg.setAttribute("id", "lightboxMedia");
-    mediaVid.setAttribute("src", mediaVideo);
 
     mediaLightbox.setAttribute("id", "mediaLightbox");
 
@@ -77,7 +81,6 @@ function mediaFactory(mediaData, photographer) {
         openLightbox(mediaImg);
       } else {
         openLightbox(mediaVid);
-        
       }
     };
 
@@ -87,16 +90,45 @@ function mediaFactory(mediaData, photographer) {
 
     function openLightbox(media) {
       lightbox.style.display = "block";
-      lightbox.append(media);
+      const lightboxImg = document.getElementById("lightboxMedia");
+      const lightboxVideo = document.getElementById("lightboxVideo");
+      if (media.tagName === "IMG") {
+        if (lightboxVideo) {
+          lightboxVideo.style.display = "none";
+        }
+        if (lightboxImg) {
+          lightboxImg.style.display = "block";
+          lightboxImg.src = media.src;
+          lightboxImg.setAttribute("mediaid", media.getAttribute("mediaid"));
+        } else {
+          lightbox.append(media);
+        }
+      } else {
+        console.log("video", media);
+        lightboxImg.style.display = "none";
+        if (lightboxVideo) {
+          lightboxVideo.style.display = "block";
+          lightboxVideo.src = media.src;
+        } else {
+          media.id = "lightboxVideo";
+          media.setAttribute("controls", "");
+          lightbox.append(media);
+        }
+      }
+      lightbox.setAttribute("mediaId", media.getAttribute("mediaid"));
     }
 
     function closeLightbox() {
       lightbox.style.display = "none";
-      lightbox.innerHTML = "";
+      // lightbox.innerHTML = "";
     }
 
-    const previousBtn = document.querySelector(".gauche");
-    const nextBtn = document.querySelector(".droite");
+   /* const previousBtn = document.querySelector(".gauche");
+    /*const nextBtn = document.querySelector(".droite");
+
+    nextBtn.addEventListener("click", function () {
+      nextMedia(true);
+    });*/
 
     // Arrow event
     document.onkeyup = function (event) {
@@ -122,16 +154,23 @@ function mediaFactory(mediaData, photographer) {
 
         if (previousMediaIndex > photographer.media.length) {
           previousMediaIndex = 0;
-          media.src ="assets/photographers/medias/" + photographer.media[0].image;
+          media.src =
+            "assets/photographers/medias/" + photographer.media[0].image;
           media.setAttribute("mediaid", photographer.media[0].id);
         } else {
-          media.src ="assets/photographers/medias/" +  photographer.media[previousMediaIndex].image;
-          media.setAttribute("mediaid", photographer.media[previousMediaIndex].id
+          media.src =
+            "assets/photographers/medias/" +
+            photographer.media[previousMediaIndex].image;
+          media.setAttribute(
+            "mediaid",
+            photographer.media[previousMediaIndex].id
           );
         }
       } else {
         previousBtn.onclick = () => {
-          const media = document.parentElement("#lightbox").querySelector("#lightboxMedia");
+          const media = document
+            .parentElement("#lightbox")
+            .querySelector("#lightboxMedia");
           const actualMediaIndex = photographer.media.findIndex(
             ({ id }) => id == media.getAttribute("mediaId")
             //
@@ -158,56 +197,35 @@ function mediaFactory(mediaData, photographer) {
     }
 
     function nextMedia(internalCall = false) {
-      if (internalCall) {
-        const media = document
-          .querySelector("#lightbox")
-          .querySelector("#lightboxMedia");
-        const actualMediaIndex = photographer.media.findIndex(
-          ({ id }) => id == media.getAttribute("mediaId")
-          //
-        );
+      // console.log("internal call", internalCall);
+      // if (internalCall) {
+      const media = document
+        .querySelector("#lightbox")
+        .querySelector("#lightboxMedia");
+      const actualMediaIndex = photographer.media.findIndex(
+        ({ id }) => id == media.getAttribute("mediaId")
+        //
+      );
+      console.log(
+        "actualMediaIndex",
+        actualMediaIndex,
+        " for ",
+        media.getAttribute("mediaId")
+      );
+      let nextMediaIndex = actualMediaIndex + 1;
 
-        let nextMediaIndex = actualMediaIndex + 1;
-
-        if (nextMediaIndex > photographer.media.length) {
-          nextMediaIndex = 0;
-          media.src =
-            "assets/photographers/medias/" + photographer.media[0].image;
-          media.setAttribute("mediaid", photographer.media[0].id);
-        } else {
-          media.src =
-            "assets/photographers/medias/" +
-            photographer.media[nextMediaIndex].image;
-          media.setAttribute("mediaid", photographer.media[nextMediaIndex].id);
-        }
+      if (nextMediaIndex > photographer.media.length) {
+        nextMediaIndex = 0;
+        media.src =
+          "assets/photographers/medias/" + photographer.media[0].image;
+        media.setAttribute("mediaid", photographer.media[0].id);
       } else {
-        nextBtn.onclick = () => {
-          const media = document
-            .querySelector("#lightbox")
-            .querySelector("#lightboxMedia");
-          const actualMediaIndex = photographer.media.findIndex(
-            ({ id }) => id == media.getAttribute("mediaId")
-            
-          );
-
-          let nextMediaIndex = actualMediaIndex + 1;
-
-          if (nextMediaIndex > photographer.media.length) {
-            nextMediaIndex = 0;
-            media.src =
-              "assets/photographers/medias/" + photographer.media[0].image;
-            media.setAttribute("mediaid", photographer.media[0].id);
-          } else {
-            media.src =
-              "assets/photographers/medias/" +
-              photographer.media[nextMediaIndex].image;
-            media.setAttribute(
-              "mediaid",
-              photographer.media[nextMediaIndex].id
-            );
-          }
-        };
+        media.src =
+          "assets/photographers/medias/" +
+          photographer.media[nextMediaIndex].image;
+        media.setAttribute("mediaid", photographer.media[nextMediaIndex].id);
       }
+      
     }
 
     // listen to the keyboard keys on the lightbox
@@ -239,10 +257,7 @@ function mediaFactory(mediaData, photographer) {
     );
     mediaLikesButton.appendChild(mediaLikesButtonImage);
 
-    // Ajout d'un like pour chaque média lorsque l'utilisateur clique sur le bouton
-   
-    previousMedia();
-    nextMedia();
+    
 
     return mediaContainer;
   }
